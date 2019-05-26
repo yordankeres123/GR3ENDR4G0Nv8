08b2051f3846425a4a9c1a265202ff4168487ea1
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-05-26 19:25:52 +0700

    Add files via upload

---
+++ GR3ENDR4G0N.sh
@@ -0,0 +1,289 @@
+clear
+hitam='\033[0;30m'
+darkdray='\033[1;30m'
+merah='\033[0;31m'
+Lmerah='\033[1;31m'
+hijau='\033[0;32m'
+Lhijau='\033[1;32m'
+brown='\033[0;33m'
+kuning='\033[1;33m'
+biru='\033[0;34m'
+Lbiru='\033[1;34m'
+ungu='\033[0;35m'
+Lungu='\033[1;35m'
+cyan='\033[0;36m'
+Lcyan='\033[1;36m'
+Lgray='\033[0;37m'
+clear
+echo $Lungu"SLAMAT DATANG YAH CUKK"
+echo
+sleep 2
+echo $Lungu"perkenalan Dulu Yah Cukk"
+echo
+sleep 2
+echo $Lungu"Siapa Nama Kamu Cuk :v"
+echo
+sleep 2
+read -p "NamaKamu : " cuok
+if [ $cuok = $cuok ]
+then
+clear
+echo $Lbiru"Salamat Datang Yah " $cuok
+echo
+sleep 2
+fi
+
+echo $Lcyan"Tools Akan Dimulai Dalam "
+echo
+sleep 1
+echo $Lcyan"Dua Detik Lagi "
+echo
+clear
+echo $biru"satu"
+echo
+sleep 2
+clear
+echo $biru"dua"
+sleep 2
+clear
+echo "Silahkan Masuk Ketoolsnya " $cuok
+sleep 3
+clear
+echo $Lbiru"___________________________________"
+sleep 00.3
+echo $Lhijau
+echo "| AUTHOR/Bigbos Starla            |"
+sleep 00.3
+echo "| MY TEAM : HAPPY HACKING TEAM    |"
+sleep 00.3
+echo "| CYB3R POP1CE +62                |"
+sleep 00.3
+echo "| FACEBOOK ME : Yordan Penceng    |"
+sleep 00.3
+echo "| Fb ke 2 Starla Starla           |"
+sleep 00.3
+echo $Lbiru"|_________________________________|"
+sleep 1
+echo $Lbiru"_____________________________"
+echo $Lhijau"| MAAF KALO GA LENGKAP      |"
+echo $Lbiru"|____________________________|"
+sleep 2
+echo $Lmerah"Tools Hanya Berisi 12 Tools Tapi"
+echo $Lmerah"Jika Dijelajahi Berisi 1000 Tools Lebih"
+sleep 2
+echo $Lmerah"SILAHKAN DIPILIH TOOLSNYA"
+sleep 2
+echo
+echo "MAAF JUGA GA RAPIH HEHEHE"
+echo $Lmerah" 1. Tools TuanB4dut "
+echo $Lmerah" 2. Tools BAJINGANv5"
+echo $Lmerah" 3. Tools BAJINGANv6"
+echo $Lmerah" 4. Tools Sekat3rs "
+echo $Lmerah" 5. Tools GadoGado"
+echo $Lbiru" 6. Webdav "
+echo $Lbiru" 7. Nyari CC buat Carding"
+echo $Lbiru" 8. SCRIPT deface creator"
+echo $Lbiru" 9. LITEFONT"
+echo $Lbiru" 10.LITESPAM "
+echo " 11.HACK FACEBOOK (Lisensi)"
+echo " 12.CLONE YAHOO[HACK FB METODE YAHOO CLONE]"
+echo $cyan" 00.Keluar "
+echo $cyan" 100. INFORMASI PEMBUAT"
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo
+echo "SILAHKAN DI PILIH"
+read -p "PILIHAN ANDA : " userr
+if [ $userr = 1 ] || [ $userr = 01 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+ls
+chmod 777 TUANB4DUT..sh
+./TUANB4DUT..sh
+fi
+
+if [ $userr = 02 ] || [ $userr = 2 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+pkg install git
+git clone https://github.com/DarkNessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+
+if [ $userr = 3 ] || [ $userr = 03 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+echo "NANTI"
+pkg install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6.git
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $userr = 4 ] || [ $userr = 04 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+git clone https://github.com/Sk4ters38/SK4T3R5v1
+cd SK4T3R5v1
+sh SK4T3RS.sh
+fi
+
+if [ $userr = 5 ] || [ $userr = 05 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+gut clone https://github.com/Senitopeng/GadoGado.git
+cd GadoGado
+unzip GadoGado.zip
+bash gado
+fi
+
+if [ $userr = 6 ] || [ $userr = 06 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+pkg install git
+git clone https://github.com/yordankeres123/webdav
+cd webdav
+sh webdav.sh
+fi
+
+if [ $userr = 7 ] || [ $userr = 07 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+cd module
+php key.php
+fi
+
+if [ $userr = 8 ] || [ $userr = 08 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $userr = 9 ] || [ $userr = 09 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+cd module
+sh LF.sh
+fi
+
+if [ $userr = 10 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $userr = 11 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+git clone https://github.com/yordankeres123/darkfb
+cd darkfb
+sh darkfb.sh
+fi
+
+if [ $userr = 12 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+echo $Lmerah"SEDANG MENGISTALL"
+git clone https://github.com/yordankeres123/cloneyahoo
+cd cloneyahoo
+sh cloning.sh
+fi
+
+if [ $userr = 00 ]
+then
+clear
+echo $Lhijau"."
+clear
+toilet -f standard -F gay "Gr3en Dr4g0n"
+sleep 3
+echo $Lhijau "SAMPAI JUMPA LAGI"
+sleep 3
+echo $Lhijau" I'M GR3EN DR4G0N"
+sleep 3
+echo $Lhijau"Chanel = Bigbos Starla"
+sleep 2
+exit
+fi
+
+if [ $userr = 100 ]
+then
+clear
+toilet -f standard -F gay "GR3EN DR4G0N"
+echo $Lbiru"PEMBUAT : Gr3en dr4g0n /Bigbos Starla"
+echo $Lbiru"FACEBOOK : Yordan Penceng / Starla Starla"
+echo $Lbiru"YOUTUBE : Bigbos Starla (Pleass Subscribe)"
+echo
+echo $Lhijau Ketik Y untuk Lanjut Ketik N untuk Keluar"
+sleep 5
+fi
+echo
+clear
+read -p "Y/N : " yaton
+if [ $yaton = Y ] || [ $yaton = y ]
+then
+clear
+sh versi8.sh
+fi
+
+if [ $yaton = N ] || [ $yaton = n ]
+then
+clear
+exit
+fi
